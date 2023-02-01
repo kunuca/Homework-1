1767e9899c144f97eca50dc7a404119af6f49bcf
Tatiana Kustova <kunuca@mail.ru>
2023-02-01 23:10:01 +0400

    006

---
+++ 006/Program.cs
@@ -0,0 +1,5 @@
+﻿// Показать последнюю цифру трёхзначного числа
+Console.WriteLine("Введите 3xзначное число");
+int number=int.Parse(Console.ReadLine());
+int x=number%10;
+Console.WriteLine("Третья цифра числа "+x);
