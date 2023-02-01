815506d8c1e2c8da8e823acd63bd1040003ffb61
Tatiana Kustova <kunuca@mail.ru>
2023-02-01 23:23:38 +0400

    008

---
+++ 008/Program.cs
@@ -0,0 +1,7 @@
+﻿// Дано число из отрезка [10, 99]. Показать наибольшую цифру числа
+Console.WriteLine("Введите число от 10 до 99");
+int number=int.Parse(Console.ReadLine());
+int x=number/10;
+int y=number%10;
+if (x>y) Console.WriteLine("Наибольшая цифра числа "+number + " - "+x);
+else Console.WriteLine("Наибольшая цифра числа "+number + " - "+y);
