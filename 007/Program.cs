68aeaf1d45cceb0b2b41960fbc9e94b1cf1e97d2
Tatiana Kustova <kunuca@mail.ru>
2023-02-01 23:16:33 +0400

    007

---
+++ 007/Program.cs
@@ -0,0 +1,7 @@
+﻿// Показать вторую цифру трёхзначного числа
+Console.WriteLine("Введите 3xзначное число");
+int number=int.Parse(Console.ReadLine());
+int x=number/10;
+int y=x%10;
+
+Console.WriteLine("Вторая цифра числа "+y);
