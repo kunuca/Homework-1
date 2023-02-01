6bbd0f45e381ae1543a9c408213edf27e7a4f80d
Tatiana Kustova <kunuca@mail.ru>
2023-02-01 21:48:10 +0400

    003

---
+++ 003/Program.cs
@@ -0,0 +1,10 @@
+﻿// Найти максимальное из трех чисел
+int[] num={2,1,-7};
+int max = num[0];
+int count=0;
+while (count<3) 
+{
+        if (max<num[count]) max=num[count];
+        count++;
+}
+Console.WriteLine(max);
