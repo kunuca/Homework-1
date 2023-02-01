d49dd2f32498fa5a107a17cd821d086908dca331
Tatiana Kustova <kunuca@mail.ru>
2023-02-01 22:41:17 +0400

    004/2

---
+++ 004/Program.cs
@@ -0,0 +1,6 @@
+﻿//Выяснить является ли число чётным
+Console.WriteLine("Введите число");
+int num=int.Parse(Console.ReadLine());
+int a=num%2;
+if (a==0) Console.WriteLine("Число четное");
+else Console.WriteLine("Число нечетное");
