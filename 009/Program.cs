1a16e2b1a400f074128552719dee8719315fbc7e
Tatiana Kustova <kunuca@mail.ru>
2023-02-01 23:29:21 +0400

    009

---
+++ 009/Program.cs
@@ -0,0 +1,7 @@
+﻿// Удалить вторую цифру трёхзначного числа
+Console.WriteLine("Введите 3xзначное число");
+int number=int.Parse(Console.ReadLine());
+int x=number/100;
+int y=number%10;
+
+Console.WriteLine("Число без второй цифры "+x+""+y);
