4a73a8436bca009fdade22d7a3893a6f1d42c5b5
Tatiana Kustova <kunuca@mail.ru>
2023-02-02 00:12:16 +0400

    011

---
+++ 011/Program.cs
@@ -0,0 +1,9 @@
+﻿// Найти третью цифру числа или сообщить, что её нет
+Console.WriteLine("Введите число");
+int number=int.Parse(Console.ReadLine());
+
+if (number>99)
+{int x=number/100;
+int y=x%10;
+Console.WriteLine("Третья цифра числа "+y);}
+else Console.WriteLine("Третьей цифры нет");
