ef83eb044df964d6871016122851871bfc9c1b6e
Tatiana Kustova <kunuca@mail.ru>
2023-02-01 23:44:22 +0400

    010

---
+++ 010/Program.cs
@@ -0,0 +1,11 @@
+﻿// Выяснить, кратно ли число заданному, если нет, вывести остаток.
+Console.WriteLine("Введите 1-е число ");
+int num1=int.Parse(Console.ReadLine());
+Console.WriteLine("Введите 2-е число ");
+int num2=int.Parse(Console.ReadLine());
+if (num1>num2)
+{
+int x=num1%num2;
+    if (x==0) Console.WriteLine("Число "+num1+" кратно числу "+num2);
+    else Console.WriteLine("Остаток "+x);}
+else Console.WriteLine("Второе число больше первого");
