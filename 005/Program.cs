bb195200f7ecd2fff05b33a9019889de393bac31
Tatiana Kustova <kunuca@mail.ru>
2023-02-01 23:01:51 +0400

    005

---
+++ 005/Program.cs
@@ -0,0 +1,12 @@
+﻿// Показать четные числа от 1 до N
+Console.WriteLine("Введите число");
+int number=int.Parse(Console.ReadLine());
+int count=1;
+int x=0;
+while (count < number) 
+ {x=count%2;
+    if (x==0) Console.Write(count+" ");
+    count++;
+ }
+x=number%2;
+if (x==0) Console.Write(number);
