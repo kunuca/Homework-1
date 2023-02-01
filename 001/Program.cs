ffe5e4cd534943131c436a571bf6414150da6772
Tatiana Kustova <kunuca@mail.ru>
2023-02-01 21:21:19 +0400

    001/2

--- 001/Program.cs
+++ 001/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//По двум заданным числам проверять является ли первое квадратом второго
+Console.WriteLine("Введите 1-е число");
+int number1=int.Parse(Console.ReadLine());
+Console.WriteLine("Введите 2-е число");
+int number2=int.Parse(Console.ReadLine());
+double s=Math.Sqrt(number1);
+if (s ==number2 ) Console.WriteLine("Является");
+else Console.WriteLine("Не является");
