469d4248cc5eedc2afbba1517473ea920b83b2f3
Tatiana Kustova <kunuca@mail.ru>
2023-02-01 21:35:51 +0400

    002

---
+++ 002/Program.cs
@@ -0,0 +1,5 @@
+﻿// По заданному номеру дня недели вывести его название
+Console.WriteLine("Введите число дня недели");
+int day=int.Parse(Console.ReadLine());
+string[] week = {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"};
+Console.WriteLine(week[day-1]);
